4d3295db0327ce73127fa22233e0a573665b9b8b
Sude <sudesavas_@hotmail.com>
2024-12-21 21:08:13 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         CountryMap map = new CountryMap();
         map.loadMap("input.txt");
 
         WayFinder finder = new WayFinder();
-        finder.findShortestPath(map.getRoutes(), map.getCityNames(), "A", "E");
-        finder.writeResult("output.txt", map.getCityNames());
-    }
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the start city: ");
+        String startCity = scanner.next();
+        System.out.println("Enter the end city: ");
+        String endCity = scanner.next();
+
+        finder.findShortestPath(map.getRoutes(), map.getCityNames(), startCity, endCity);
+        finder.writeResult("output.txt", map.getCityNames()); }
 }
