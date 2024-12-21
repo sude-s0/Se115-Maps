634f4748a2cb58afd3f9f524f9d8e8c237b5dd45
Sude <sudesavas_@hotmail.com>
2024-12-21 21:33:13 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,9 +14,19 @@ public class Main {
 
         System.out.print("Enter the start city: ");
         String startCity = scanner.next();
-        System.out.println("Enter the end city: ");
+        System.out.print("Enter the end city: ");
         String endCity = scanner.next();
 
         finder.findShortestPath(map.getRoutes(), map.getCityNames(), startCity, endCity);
-        finder.writeResult("output.txt", map.getCityNames()); }
+        finder.writeResult("output.txt", map.getCityNames());
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader("output.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
+    }
 }
